87deeec52248771a1d114bdc6c3aea6ec8801e5e
Leonardo Lopez <lopez.leonardo.info@gmail.com>
2019-04-01 12:25:26 -0300

    task1 casi terminada

--- main.js
+++ main.js
@@ -57,3 +57,73 @@ for (var i = 0; i < ageArray.length; i++) {
     console.log(ageArray[i]);
   }
 }
+//Exercise 3
+function numeroBajo(Array) {
+  Array.sort();
+  console.log(Array[0]);
+}
+numeroBajo(ageArray);
+
+//Exercise 4
+function numeroAlto(Array) {
+  Array.sort();
+  Array.reverse();
+  console.log(Array[0]);
+}
+numeroAlto(ageArray);
+
+//Exercise 5
+var array = [3, 6, 67, 6, 23, 11, 100, 8, 93, 0, 17, 24, 7, 1, 33, 45, 28, 33, 23, 12, 99, 100];
+var index = 1;
+
+function numeroIndice(Array, indice) {
+  console.log(Array[indice]);
+}
+
+numeroIndice(array, index);
+
+//Exercise 6
+var array2 = [3, 6, 67, 6, 23, 11, 100, 8, 93, 0, 17, 24, 7, 1, 33, 45, 28, 33, 23, 12, 99, 100];
+
+function valorRepetido(Array) {
+  Array.sort();
+  let array3 = [];
+  for (var i = 0; i < Array.length; i++) {
+    if (Array[i] != Array[i + 1]) {} else {
+      array3.push(Array[i]);
+    }
+  }
+  console.log(array3);
+}
+valorRepetido(array2);
+
+//Exercise 7
+myColor = ["Red", "Green", "White", "Black"];
+console.log(myColor.toString());
+
+//String Exercise 1
+var x = 32443;
+
+function reverse(number) {
+  number = number + "";
+  return number.split("").reverse().join("");
+}
+console.log(reverse(x));
+
+//Exercise 2
+var x = 'webmaster';
+
+function orderAlphabetical(string) {
+  return(string.split("").sort().join(""));
+}
+
+console.log(orderAlphabetical(x));
+
+//Exercise 3
+var x = "prince of persia";
+
+function uppercase(string) {
+  return (string.replace(/\b\w/g, l => l.toUpperCase()));
+}
+
+console.log(uppercase(x));
