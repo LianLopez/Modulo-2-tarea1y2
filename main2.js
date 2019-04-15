b0f26805616f6b92144f8ec88f70bcdec3fd1ef6
Leonardo Lopez <lopez.leonardo.info@gmail.com>
2019-04-15 12:11:30 -0300

    point 5 finished

--- main2.js
+++ main2.js
@@ -1,7 +1,18 @@
-var obj, table = "";
-datasenate = JSON.stringify(datasenate);
-function printData(element){
-  
+function renderTable(data){
+  var html = renderRows(data);
+  document.getElementById("senate-data").innerHTML = html;
 }
 
-document.getElementById("senate-data").innerHTML = datasenate.forEach(printData);
+function renderRows(data) {
+    return data.results.map(function(element){
+      return element.members.map(function(element){
+        var html = "";
+        html += "<tr><td>" + element.first_name; 
+        element.middle_name == null ? html += " " : html += " " + element.middle_name + " ";  
+        html += element.last_name + "</td><td>" + element.party + "</td><td>" + element.state + "</td><td>" + element.seniority + "</td><td>" + element.votes_with_party_pct + "%</td></tr>";
+        return html;
+      }).join("");
+  });
+}
+
+renderTable(datasenate);
