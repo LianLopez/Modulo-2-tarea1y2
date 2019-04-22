aff67026cc67f38d032ce36e7543c9933d5687e7
Leonardo Lopez <lopez.leonardo.info@gmail.com>
2019-04-22 12:11:28 -0300

    inicio de bootstrap

--- js/main2.js
+++ js/main2.js
@@ -7,12 +7,15 @@ function renderRows(data) {
     return data.results.map(function(element){
       return element.members.map(function(element){
         var html = "";
-        html += "<tr><td>" + element.first_name; 
+        html += "<tr><td><a href='"+ element.url +"'>" + element.first_name; 
         element.middle_name == null ? html += " " : html += " " + element.middle_name + " ";  
-        html += element.last_name + "</td><td>" + element.party + "</td><td>" + element.state + "</td><td>" + element.seniority + "</td><td>" + element.votes_with_party_pct + "%</td></tr>";
+        html += element.last_name + "</a></td><td>" + element.party + "</td><td>" + element.state + "</td><td>" + element.seniority + "</td><td>" + element.votes_with_party_pct + "%</td></tr>";
         return html;
       }).join("");
   });
 }
 
 renderTable(datasenate);
+
+
+
