5ea11aeecf6bc5cad70578a4c9e303ccef70445e
LianLopez <lopez.leonardo.info@gmail.com>
2019-05-06 11:33:28 -0300

    task2 finished

--- js/senate.js
+++ js/senate.js
@@ -1,69 +1,43 @@
-var info = "";
-
 function eliminarFila() {
-	var fin = true;
-	while (Boolean(fin) == true) {
+	var fin = true
+	while (fin) {
 		if (document.getElementById("fila")) {
-			$("#fila").remove();
+			$("#fila").remove()
 		} else {
-			fin = false;
+			fin = false
 		}
 	}
 }
 
 
-function renderTableFiltered(data, party, state) {
-	for (let i = 0; i < party.length; i++) {
-		if (state.length > 0) {
-			if (party[i].checked) {
-				info += data.results.map(function (element) {
-					return element.members.map(function (element) {
-						if (element.party == party[i].value && state == element.state) {
-							var html = "";
-							html += "<tr id='fila'><td scope='row'><a href='" + element.url + "'>" + element.first_name;
-							element.middle_name == null ? html += " " : html += " " + element.middle_name + " ";
-							html += element.last_name + "</a></td><td>" + element.party + "</td><td>" + element.state + "</td><td>" + element.seniority + "</td><td>" + element.votes_with_party_pct + "%</td></tr>";
-							return html;
-						}
-					}).join("");
-				});
-			}
-		} else {
-			if (party[i].checked) {
-				info += data.results.map(function (element) {
-					return element.members.map(function (element) {
-						if (element.party == party[i].value) {
-							var html = "";
-							html += "<tr id='fila'><td scope='row'><a href='" + element.url + "'>" + element.first_name;
-							element.middle_name == null ? html += " " : html += " " + element.middle_name + " ";
-							html += element.last_name + "</a></td><td>" + element.party + "</td><td>" + element.state + "</td><td>" + element.seniority + "</td><td>" + element.votes_with_party_pct + "%</td></tr>";
-							return html;
-						}
-					}).join("");
-				});
-			}
-		}
-	}
-	return info;
+function renderTableFiltered(listaFiltrada) {
+
+	return listaFiltrada.map(function (element) {
+		var html = ""
+		html += "<tr id='fila'><td scope='row'><a href='" + element.url + "'>" + element.first_name
+		element.middle_name == null ? html += " " : html += " " + element.middle_name + " "
+		html += element.last_name + "</a></td><td>" + element.party + "</td><td>" + element.state + "</td><td>" + element.seniority + "</td><td>" + element.votes_with_party_pct + "%</td></tr>"
+		return html;
+	}).join("")
+}
+
+function miFiltro(listacompleta) {
+	var chekeados = Array.from(document.querySelectorAll("input[name=party]:checked")).map(input => input.value)
+	console.log(chekeados)
+	var selected = document.querySelector("select").value;
+	console.log(selected)
+
+	listaFiltrada = listacompleta.filter(member => chekeados.includes(member.party) && (selected == "" ? true : selected == member.state))
+	console.log(listaFiltrada)
+	return listaFiltrada
+
 }
 
 function obtenerValores() {
 	if (document.getElementById("tabla")) {
-		var republican = document.getElementById("R");
-		var democrat = document.getElementById("D");
-		var independient = document.getElementById("I");
-		var party = [republican, democrat, independient];
-		var state = document.getElementById("selectstate").value;
-
-		eliminarFila();
-		var html = renderTableFiltered(data, party, state);
-		document.getElementById("tabla").innerHTML = html;
-		info = "";
+		eliminarFila()
+		var html = renderTableFiltered(miFiltro(data.results[0].members))
+		document.getElementById("tabla").innerHTML = html
 	}
 }
-
-window.onload = function () {
-	obtenerValores();
-	console.log("Pagina cargada");
-	
-}
+obtenerValores()
